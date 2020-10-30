d5e01a091fbe3d414c65c108aa416d5fea8bec14
José Douglas Pontes Silva <37352134+IndioP@users.noreply.github.com>
2020-10-30 09:47:00 -0300

    python code to create the dataset

---
+++ geraDataSet.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+import os
+
+from string import ascii_lowercase
+
+try:
+    os.mkdir('DATASET')
+except OSError:
+    print ("Creation of the directory  failed" )
+else:
+    print ("Successfully created the directory")
+
+cap = cv2.VideoCapture(0)
+
+
+i = 0
+
+alfabeto = iter(ascii_lowercase)
+caractere = next(alfabeto)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    frame = cv2.rectangle(frame,(100,200),(300,400),(255,255,255),10)
+    cv2.putText(frame, caractere,(50, 50),cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 255, 255),2,cv2.LINE_4)
+
+    cv2.imshow('frame',frame)
+
+    frame = frame[100:300,200:400]
+    # Display the resulting frame
+    key = cv2.waitKey(33)
+    if key == ord('s'):
+   		print "saving cropped frame to dataset"
+   		for i in range(10):
+   			for _ in range(4):
+				ret, frame = cap.read()
+			frame = cv2.rectangle(frame,(100,200),(300,400),(255,255,255),10)
+			cv2.putText(frame, caractere,(50, 50),cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 255, 255),2,cv2.LINE_4)
+
+			cv2.imshow('frame',frame)
+
+			frame = frame[100:300,200:400]
+			cv2.imwrite('DATASET/'+caractere+str(i)+'.jpg',frame)
+			i += 1
+		i = 0	
+		caractere = next(alfabeto)
+
+    elif key & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
