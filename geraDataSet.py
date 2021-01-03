2359caefa99e0db925fbf4d4a14322f3379dfc2a
Douglas Pontes <jdps@cin.ufpe.br>
2021-01-03 10:58:41 -0300

    use this to make the csv dataset

--- geraDataSet.py
+++ geraDataSet.py
@@ -3,7 +3,7 @@ import cv2
 import os
 import time
 
-import json
+import csv
 
 import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
@@ -54,7 +54,9 @@ writing = False
 alfabeto = iter(ascii_lowercase)
 caractere = next(alfabeto)
 #makeDir('DATASET/'+caractere)
-f = open('DATASET/'+caractere,"w")
+csv_file = open('DATASET/'+caractere+'.csv',"w")
+f = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+f.writerow(["left_or_right","WRIST.x","WRIST.y","WRIST.z","THUMB_CMC.x","THUMB_CMC.y","THUMB_CMC.z","THUMB_MCP.x","THUMB_MCP.y","THUMB_MCP.z","THUMB_IP.x","THUMB_IP.y","THUMB_IP.z","THUMB_TIP.x","THUMB_TIP.y","THUMB_TIP.z","INDEX_FINGER_MCP.x","INDEX_FINGER_MCP.y","INDEX_FINGER_MCP.z","INDEX_FINGER_PIP.x","INDEX_FINGER_PIP.y","INDEX_FINGER_PIP.z","INDEX_FINGER_DIP.x","INDEX_FINGER_DIP.y","INDEX_FINGER_DIP.z","INDEX_FINGER_TIP.x","INDEX_FINGER_TIP.y","INDEX_FINGER_TIP.z","MIDDLE_FINGER_MCP.x","MIDDLE_FINGER_MCP.y","MIDDLE_FINGER_MCP.z","MIDDLE_FINGER_PIP.x","MIDDLE_FINGER_PIP.y","MIDDLE_FINGER_PIP.z","MIDDLE_FINGER_DIP.x","MIDDLE_FINGER_DIP.y","MIDDLE_FINGER_DIP.z","MIDDLE_FINGER_TIP.x","MIDDLE_FINGER_TIP.y","MIDDLE_FINGER_TIP.z","RING_FINGER_MCP.x","RING_FINGER_MCP.y","RING_FINGER_MCP.z","RING_FINGER_PIP.x","RING_FINGER_PIP.y","RING_FINGER_PIP.z","RING_FINGER_DIP.x","RING_FINGER_DIP.y","RING_FINGER_DIP.z","RING_FINGER_TIP.x","RING_FINGER_TIP.y","RING_FINGER_TIP.z","PINKY_MCP.x","PINKY_MCP.y","PINKY_MCP.z","PINKY_PIP.x","PINKY_PIP.y","PINKY_PIP.z","PINKY_DIP.x","PINKY_DIP.y","PINKY_DIP.z","PINKY_TIPPINKY_TIP.x","PINKY_TIPPINKY_TIP.y","PINKY_TIPPINKY_TIP.z"])
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -75,12 +77,11 @@ while(True):
     frame = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
     results = findHands(frame)
     
-    if results.multi_handedness != None:
-        print(results.multi_handedness[0].classification[0].label)
-        print(results.multi_hand_landmarks[0])
+
     if writing == True:
         print ("saving cropped frame to dataset")
-        if i < 10:
+        if i < 12:
+            
             elapsed = int(time.time()) - startTime
             if elapsed < 3:
                 cv2.putText(frame, "saving in "+str(3-elapsed),(200, 50),cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 255, 255),2,cv2.LINE_4)
@@ -88,29 +89,30 @@ while(True):
 
             else:
                 startTime = int(time.time())
-                for j in range(10):
-                    ret, frame = cap.read()
-                    frame = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
-                    results = findHands(frame)
-                    show(frame,(0,0,255),results)
-                    #f.write(results.multi_handedness[0].classification[0].label)
-                    #print(type(list(results.multi_hand_landmarks)))
-                    l = list(map(lambda x: str(x),results.multi_hand_landmarks))
-                    jstr = json.dumps(l, indent=4)
-                    f.write(jstr)
-                i += 1
+                
+                if results.multi_handedness != None:
+                    points = [results.multi_handedness[0].classification[0].label]
+                    for l in results.multi_hand_landmarks[0].landmark:
+                        points.append(l.x)
+                        points.append(l.y)
+                        points.append(l.z)
+                    f.writerow(points)
+                    writing = False
+                    i += 1
         else:
             i = 0
             writing = False
             caractere = next(alfabeto)
-            f.close()
-            f = open('DATASET/'+caractere,"w")
+            csv_file.close()
+            csv_file = open('DATASET/'+caractere+'.csv',"w")
+            f = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            f.writerow(["left_or_right","WRIST.x","WRIST.y","WRIST.z","THUMB_CMC.x","THUMB_CMC.y","THUMB_CMC.z","THUMB_MCP.x","THUMB_MCP.y","THUMB_MCP.z","THUMB_IP.x","THUMB_IP.y","THUMB_IP.z","THUMB_TIP.x","THUMB_TIP.y","THUMB_TIP.z","INDEX_FINGER_MCP.x","INDEX_FINGER_MCP.y","INDEX_FINGER_MCP.z","INDEX_FINGER_PIP.x","INDEX_FINGER_PIP.y","INDEX_FINGER_PIP.z","INDEX_FINGER_DIP.x","INDEX_FINGER_DIP.y","INDEX_FINGER_DIP.z","INDEX_FINGER_TIP.x","INDEX_FINGER_TIP.y","INDEX_FINGER_TIP.z","MIDDLE_FINGER_MCP.x","MIDDLE_FINGER_MCP.y","MIDDLE_FINGER_MCP.z","MIDDLE_FINGER_PIP.x","MIDDLE_FINGER_PIP.y","MIDDLE_FINGER_PIP.z","MIDDLE_FINGER_DIP.x","MIDDLE_FINGER_DIP.y","MIDDLE_FINGER_DIP.z","MIDDLE_FINGER_TIP.x","MIDDLE_FINGER_TIP.y","MIDDLE_FINGER_TIP.z","RING_FINGER_MCP.x","RING_FINGER_MCP.y","RING_FINGER_MCP.z","RING_FINGER_PIP.x","RING_FINGER_PIP.y","RING_FINGER_PIP.z","RING_FINGER_DIP.x","RING_FINGER_DIP.y","RING_FINGER_DIP.z","RING_FINGER_TIP.x","RING_FINGER_TIP.y","RING_FINGER_TIP.z","PINKY_MCP.x","PINKY_MCP.y","PINKY_MCP.z","PINKY_PIP.x","PINKY_PIP.y","PINKY_PIP.z","PINKY_DIP.x","PINKY_DIP.y","PINKY_DIP.z","PINKY_TIPPINKY_TIP.x","PINKY_TIPPINKY_TIP.y","PINKY_TIPPINKY_TIP.z"])
             #makeDir('DATASET/'+caractere)	
     else:
         show(frame,(255,255,255),results)
 
 # When everything done, release the capture
-f.close()
+csv_file.close()
 hands.close()
 cap.release()
 cv2.destroyAllWindows()
